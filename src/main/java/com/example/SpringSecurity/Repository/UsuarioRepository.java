3b682ea890ab5101fcc39790866325d74ca3c7da
Eduardo-Souza2000 <eduardosouza.foz2014@outlook.com>
2023-11-08 15:39:10 -0300

    Feito o service de Usuario

--- src/main/java/com/example/SpringSecurity/Repository/UsuarioRepository.java
+++ src/main/java/com/example/SpringSecurity/Repository/UsuarioRepository.java
@@ -1,4 +1,8 @@
 package com.example.SpringSecurity.Repository;
 
-public interface UsuarioRepository {
+import com.example.SpringSecurity.Entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
 }
--- src/main/java/com/example/SpringSecurity/Service/UsuarioService.java
+++ src/main/java/com/example/SpringSecurity/Service/UsuarioService.java
@@ -1,4 +1,61 @@
 package com.example.SpringSecurity.Service;
 
+import com.example.SpringSecurity.DTO.UsuarioDTO;
+import com.example.SpringSecurity.Entity.Usuario;
+import com.example.SpringSecurity.Repository.UsuarioRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.example.SpringSecurity.config.message.handling.ErrorMessages;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.ErrorManager;
+
+@Service
 public class UsuarioService {
+    @Autowired
+    UsuarioRepository repository;
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public UsuarioDTO findById(Long id){
+
+        Usuario usuario = this.repository.findById(id).orElseThrow(()-> {
+            throw new EntityNotFoundException(ErrorMessages.ENTIDADE_NAO_ENCONTRADA);
+        });
+
+        return modelMapper.map(usuario, UsuarioDTO.class);
+    }
+
+
+    public List<UsuarioDTO> findAll(){
+        List<Usuario> usuarios = this.repository.findAll();
+        List<UsuarioDTO> usuarioDTOS = new ArrayList<>();
+        for (Usuario i : usuarios){
+            usuarioDTOS.add(modelMapper.map(i,UsuarioDTO.class));
+        }
+        return usuarioDTOS;
+    }
+
+    public void cadastrar(UsuarioDTO usuarioDTO){
+        this.repository.save(modelMapper.map(usuarioDTO, Usuario.class));
+    }
+
+    public void alterar(Long id, UsuarioDTO  usuarioDTO){
+        Usuario usuario = this.repository.findById(id).orElseThrow(() ->
+                new RuntimeException("Registro não encontrado"));
+        modelMapper.map(usuarioDTO, usuario);
+        this.repository.save(usuario);
+    }
+
+   /* public void delete (Long id){
+        Usuario usuario = this.repository.findById(id).orElseThrow(()->
+                new RuntimeException("Registro não encontrado"));
+        usuario.setAtivo(false);
+
+    }*/
+
 }
